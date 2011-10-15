f0f0d5b309b115f23510cc95973c894908d3534a
ryan.c.diener@gmail.com <ryan.c.diener@gmail.com@4b4c9caf-8d78-7f06-f573-cc5358e76b87>
2011-10-15 15:31:58 +0000

    2011/10/15 Ryan Diener: This is the first check-in.

---
+++ trunk/wsu-treasureland-hotel/TreasureLand/TreasureLand/Default.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.Linq;
+
+namespace TreasureLand
+{
+    public partial class _Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+             ///This is just a test to make sure I can get some data
+            TreasureLandDataClassesDataContext db = new TreasureLandDataClassesDataContext();
+            var customersInDb = from c in db.Guests select c;
+            int customerCount = customersInDb.Count();
+            ///This is just a test to make sure I can get some data
+        }
+    }
+}
