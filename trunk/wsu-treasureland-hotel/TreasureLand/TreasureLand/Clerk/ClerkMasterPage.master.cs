86a4fd5f56be9dea5953a6d32e255874a169a8ef
jasonlnightengale@gmail.com <jasonlnightengale@gmail.com@4b4c9caf-8d78-7f06-f573-cc5358e76b87>
2011-10-22 01:37:38 +0000

    Created the Clerk Master Page and Content Pages for the Clerk
    Jason Nightengale

---
+++ trunk/wsu-treasureland-hotel/TreasureLand/TreasureLand/Clerk/ClerkMasterPage.master.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace TreasureLand.Clerk
+{
+    public partial class ClerkMasterPage : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnHome_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("ClerkDefault.aspx");
+        }
+
+        protected void btnCheckIn_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("CheckIn.aspx");
+        }
+
+        protected void Checkout_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("CheckOut.aspx");
+        }
+
+        protected void btnUpdateGuestFolio_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("UpdateGuestFolio.aspx");
+        }
+
+        protected void btnCreateReservation_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("CreateReservation.aspx");
+        }
+
+        protected void btnUpdateReservation_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("UpdateReservation.aspx");
+        }
+
+        protected void btnViewGuest_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("ViewGuests.aspx");
+        }
+
+        protected void btnReports_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("Reports.aspx");
+        }
+    }
+}
