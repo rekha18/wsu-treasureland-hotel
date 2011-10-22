d99b06abd4d5225962467f3949958635f70c4f54
jasonlnightengale@gmail.com <jasonlnightengale@gmail.com@4b4c9caf-8d78-7f06-f573-cc5358e76b87>
2011-10-22 22:04:02 +0000

    Added the Treasureland Logo to the Master Page

--- trunk/wsu-treasureland-hotel/TreasureLand/TreasureLand/Site.Master.designer.cs
+++ trunk/wsu-treasureland-hotel/TreasureLand/TreasureLand/Site.Master.designer.cs
@@ -22,6 +22,15 @@ namespace TreasureLand {
         protected global::System.Web.UI.WebControls.ContentPlaceHolder HeadContent;
         
         /// <summary>
+        /// imageLogo control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Image imageLogo;
+        
+        /// <summary>
         /// HeadLoginView control.
         /// </summary>
         /// <remarks>
