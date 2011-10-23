d6dd3a26a9323ed61f08dc49af15a51797adad0e
littlevalder <littlevalder@gmail.com>
2011-10-23 04:44:29 +0000

    Modified Master to include a manage discounts button

--- wsu-treasureland-hotel/TreasureLand/TreasureLand/Admin/AdminMaster.master.designer.cs
+++ wsu-treasureland-hotel/TreasureLand/TreasureLand/Admin/AdminMaster.master.designer.cs
@@ -49,6 +49,15 @@ namespace TreasureLand.Admin {
         protected global::System.Web.UI.WebControls.Button btnManageRooms;
         
         /// <summary>
+        /// btnManageDiscounts control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnManageDiscounts;
+        
+        /// <summary>
         /// btnManageUsers control.
         /// </summary>
         /// <remarks>
