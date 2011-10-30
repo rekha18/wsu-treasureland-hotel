e3462d17f51a196daf743ce1e2fb6f2783318455
jimmywhip <jimmywhip@gmail.com>
2011-10-30 13:59:03 +0000

    Added the date range view and shell UI

--- wsu-treasureland-hotel/TreasureLand/TreasureLand/Clerk/UpdateReservation.aspx
+++ wsu-treasureland-hotel/TreasureLand/TreasureLand/Clerk/UpdateReservation.aspx
@@ -1,5 +1,81 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Clerk/ClerkMasterPage.master" AutoEventWireup="true" CodeBehind="UpdateReservation.aspx.cs" Inherits="TreasureLand.Clerk.WebForm6" %>
 <asp:Content ID="Content1" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
-    <p>
-        Update delete reservation</p>
+    <table>
+    <tr>
+        <td>
+            <asp:Calendar ID="calDatePicker" runat="server" 
+                onselectionchanged="calDatePicker_SelectionChanged"></asp:Calendar>
+        </td>
+        <td>
+            <div id="dateselectors" style="width:inherit; margin-left: auto; margin-right: auto; text-align:center;">
+                <span style="padding: 0px 5px 5px 5px;">
+                    <asp:LinkButton ID="lbtnDatePrevious" runat="server" onclick="lbtnPrevious_Click"></asp:LinkButton>
+                </span>
+                <span style="padding: 0px 5px 5px 5px;">
+                    <asp:LinkButton ID="lbtnToday" runat="server" onclick="lbtnToday_Click">Today</asp:LinkButton>
+                </span>
+                <span style="padding: 0px 5px 5px 5px;">
+                    <asp:LinkButton ID="lbtnDateFuture" runat="server" onclick="lbtnFuture_Click"></asp:LinkButton>
+                </span>
+            </div>
+            <div id="table" style="width:inherit; margin-left: auto; margin-right: auto; text-align:center;">
+                <asp:Label ID="lblTable" runat="server" onprerender="generateTable"></asp:Label>
+            </div>
+            <div id="pageselectors" style="width:inherit; margin-left: auto; margin-right: auto; text-align:center;">
+                <span style="padding: 0px 5px 5px 5px;">
+                    <asp:LinkButton ID="lbtnPagePrevious" runat="server" 
+                    onclick="lbtnPagePrevious_Click"></asp:LinkButton>
+                </span>
+                <span style="padding: 0px 5px 5px 5px;">
+                    <asp:LinkButton ID="lbtnPageNext" runat="server" 
+                    onclick="lbtnPageNext_Click"></asp:LinkButton>
+                </span>
+            </div>
+        </td>
+    </tr>
+    <tr><!-- New Page Code Starts Here -->
+        <td colspan="2">
+            <p>Add Current Reservation</p>
+            <div id="Div1">
+                <span style="padding: 5px 5px 0px 0px">Highlight rooms of type:</span>
+                <asp:DropDownList ID="ddlRoomTypes" runat="server">
+                </asp:DropDownList>
+            </div>
+        </td>
+    </tr>
+    <tr>
+        <td>
+            <div id="AddReservation">
+                <span style="padding: 5px 5px 0px 0px">Room #:</span>
+                <asp:TextBox ID="txtRoomNumber" runat="server"></asp:TextBox>
+                <span style="padding: 5px 25px 0px 0px"></span>
+                <asp:Button ID="btnSelectRoom" runat="server" Text="Button" />
+            </div>
+        </td>
+    </tr>
+</table>
+    <script type="text/javascript">
+    <!--
+
+        function select(room) {
+            document.getElementById("row" + room).style.backgroundColor = "#7CC4FF";
+
+            for (var i = 0; i < 7; i++) {
+                var id = document.getElementById("room" + room + "col" + i);
+                id.style.color = "#008CFF";
+                //id.style.fontWeight = 'bold';
+            }
+        }
+
+        function deselect(room) {
+            document.getElementById("row" + room).style.backgroundColor = "#AAAAAA";
+
+            for (var i = 0; i < 7; i++) {
+                var id = document.getElementById("room" + room + "col" + i);
+                id.style.color = "#696969";
+                //id.style.fontWeight = 'normal';
+            }
+        }
+    -->
+    </script>
 </asp:Content>
--- wsu-treasureland-hotel/TreasureLand/TreasureLand/Clerk/UpdateReservation.aspx.cs
+++ wsu-treasureland-hotel/TreasureLand/TreasureLand/Clerk/UpdateReservation.aspx.cs
@@ -4,14 +4,116 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using TreasureLand.App_Code;
 
 namespace TreasureLand.Clerk
 {
     public partial class WebForm6 : System.Web.UI.Page
     {
+        private GridRangeView cHome = new GridRangeView();
+        private bool requiresUpdate = true;
+
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        /// <summary>
+        /// Generates the GridRangeView object before the HTML page is
+        /// rendered.
+        /// </summary>
+        /// <param name="sender">lbtnTable</param>
+        /// <param name="e"></param>
+        protected void generateTable(object sender, EventArgs e)
+        {
+            if (requiresUpdate)
+                cHome.update();
+            lblTable.Text = cHome.generateTableHTMLv2(true);
+
+            //Generate label information
+            lbtnDatePrevious.Text = "Previous " + GridRangeView.DaysDisplayed + " Days";
+            lbtnDateFuture.Text = "Next " + GridRangeView.DaysDisplayed + " Days";
+            lbtnPageNext.Text = "Next " + GridRangeView.PageSize + " Rooms";
+            lbtnPagePrevious.Text = "Previous " + GridRangeView.PageSize + " Rooms";
+
+            if (GridRangeView.RoomIndex < GridRangeView.PageSize)
+                lbtnPagePrevious.Enabled = false;
+            else
+                lbtnPagePrevious.Enabled = true;
+
+            if (GridRangeView.RoomIndex + GridRangeView.PageSize >= GridRangeView.MaxRooms)
+                lbtnPageNext.Enabled = false;
+            else
+                lbtnPagePrevious.Enabled = true;
+
+            requiresUpdate = false;
+        }
+
+        /// <summary>
+        /// Decrements the current date by the DaysDisplayed
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public void lbtnPrevious_Click(object sender, EventArgs e)
+        {
+            GridRangeView.current = GridRangeView.current.AddDays(-GridRangeView.DaysDisplayed);
+            requiresUpdate = true;
+        }
+
+        /// <summary>
+        /// Increments the current date by the DaysDisplayed
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public void lbtnFuture_Click(object sender, EventArgs e)
+        {
+            GridRangeView.current = GridRangeView.current.AddDays(GridRangeView.DaysDisplayed);
+            requiresUpdate = true;
+        }
+
+        /// <summary>
+        /// Sets the current date to the current time
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public void lbtnToday_Click(object sender, EventArgs e)
+        {
+            GridRangeView.current = DateTime.Now.Date;
+            requiresUpdate = true;
+        }
+
+        /// <summary>
+        /// Sets the paging controls back by the number of room defined in the GridRangeView class
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        protected void lbtnPagePrevious_Click(object sender, EventArgs e)
+        {
+            GridRangeView.RoomIndex -= GridRangeView.PageSize;
+            requiresUpdate = true;
+        }
+
+        /// <summary>
+        /// Sets the paging controls forward by the number of room defined in 
+        /// the GridRangeView class
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        protected void lbtnPageNext_Click(object sender, EventArgs e)
+        {
+            GridRangeView.RoomIndex += GridRangeView.PageSize;
+            requiresUpdate = true;
+        }
+
+        /// <summary>
+        /// Sets the GridRangeView table to display a specific date
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        protected void calDatePicker_SelectionChanged(object sender, EventArgs e)
+        {
+            GridRangeView.current = calDatePicker.SelectedDate;
+            requiresUpdate = true;
         }
     }
 }
