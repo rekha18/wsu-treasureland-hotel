b621f03b28a8f3dfbf65ba17c8388535a14f19a0
littlevalder@gmail.com <littlevalder@gmail.com@4b4c9caf-8d78-7f06-f573-cc5358e76b87>
2011-11-01 22:43:39 +0000

    Fixed manage services button

--- trunk/wsu-treasureland-hotel/TreasureLand/TreasureLand/Admin/AdminMaster.master.designer.cs
+++ trunk/wsu-treasureland-hotel/TreasureLand/TreasureLand/Admin/AdminMaster.master.designer.cs
@@ -76,13 +76,13 @@ namespace TreasureLand.Admin {
         protected global::System.Web.UI.WebControls.Button btnManageStatuses;
         
         /// <summary>
-        /// btnManageBillingCategories control.
+        /// btnManageServices control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Button btnManageBillingCategories;
+        protected global::System.Web.UI.WebControls.Button btnManageServices;
         
         /// <summary>
         /// adminContentHolder control.
