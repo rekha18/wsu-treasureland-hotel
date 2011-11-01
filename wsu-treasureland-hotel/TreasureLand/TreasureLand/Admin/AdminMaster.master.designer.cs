6bf2164b63f760cc030651abb39604b2472433a0
littlevalder <littlevalder@gmail.com>
2011-11-01 23:20:13 +0000

    Fixed Manage Billing Category page

--- wsu-treasureland-hotel/TreasureLand/TreasureLand/Admin/AdminMaster.master.designer.cs
+++ wsu-treasureland-hotel/TreasureLand/TreasureLand/Admin/AdminMaster.master.designer.cs
@@ -76,13 +76,13 @@ namespace TreasureLand.Admin {
         protected global::System.Web.UI.WebControls.Button btnManageStatuses;
         
         /// <summary>
-        /// btnManageServices control.
+        /// btnManageServicesFees control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Button btnManageServices;
+        protected global::System.Web.UI.WebControls.Button btnManageServicesFees;
         
         /// <summary>
         /// adminContentHolder control.
