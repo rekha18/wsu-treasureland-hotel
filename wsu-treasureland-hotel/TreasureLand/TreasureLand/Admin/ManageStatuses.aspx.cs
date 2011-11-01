ee7cd02fe64dbefdc76be0133ee684e0828f45c5
littlevalder <littlevalder@gmail.com>
2011-11-01 21:41:32 +0000

    Changed Manage Services to Manage Statuses Services can be changed in the manage billing categories (Will probably change that to services).

---
+++ wsu-treasureland-hotel/TreasureLand/TreasureLand/Admin/ManageStatuses.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace TreasureLand.Admin
+{
+    public partial class ManageStatuses : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnAddStatuses_Click(object sender, EventArgs e)
+        {
+            TreasureLandDataClassesDataContext db = new TreasureLandDataClassesDataContext();
+            RoomStatus status = new RoomStatus();
+            status.Description = txtDescription.Text;
+            db.RoomStatus.InsertOnSubmit(status);
+            db.SubmitChanges();
+        }
+    }
+}
