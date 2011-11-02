f81a4eaa5fc7063ae7aa25a1954bad2e7054f069
spyderweiss@gmail.com <spyderweiss@gmail.com@4b4c9caf-8d78-7f06-f573-cc5358e76b87>
2011-11-02 04:26:04 +0000

    File is no longer necessary.

--- trunk/wsu-treasureland-hotel/TreasureLand/TreasureLand/Clerk/CheckOut.aspx.cs
+++ trunk/wsu-treasureland-hotel/TreasureLand/TreasureLand/Clerk/CheckOut.aspx.cs
@@ -1,17 +1,1 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
 
-namespace TreasureLand.Clerk
-{
-	public partial class CheckOut : System.Web.UI.Page
-	{
-		protected void Page_Load(object sender, EventArgs e)
-		{
-
-		}
-	}
-}
