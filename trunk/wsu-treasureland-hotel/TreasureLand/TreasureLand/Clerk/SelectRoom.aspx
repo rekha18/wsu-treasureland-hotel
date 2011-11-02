42855f91c30b7001860bdd072385c470c12f54f8
jimmywhip@gmail.com <jimmywhip@gmail.com@4b4c9caf-8d78-7f06-f573-cc5358e76b87>
2011-11-02 19:52:34 +0000

    Allows a user to select a room for a new reservation, or move an existing reservation's room to a new room.

---
+++ trunk/wsu-treasureland-hotel/TreasureLand/TreasureLand/Clerk/SelectRoom.aspx
@@ -0,0 +1,127 @@
+﻿<%@ Page Title="Select a room" Language="C#" MasterPageFile="~/Clerk/ClerkMasterPage.master" AutoEventWireup="true" CodeBehind="SelectRoom.aspx.cs" Inherits="TreasureLand.Clerk.SelectRoom" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+<table>
+    <tr>
+        <td>
+            <asp:Calendar ID="calDatePicker" runat="server" 
+                onselectionchanged="calDatePicker_SelectionChanged"></asp:Calendar>
+        </td>
+        <td>
+            <div id="dateselectors" style="width:inherit; margin-left: auto; margin-right: auto; text-align:center;">
+                <span style="padding: 0px 5px 5px 5px;">
+                    <asp:LinkButton ID="lbtnDatePrevious" runat="server" onclick="lbtnPrevious_Click"></asp:LinkButton>
+                </span>
+                <span style="padding: 0px 5px 5px 5px;">
+                    <asp:LinkButton ID="lbtnToday" runat="server" onclick="lbtnToday_Click">Today</asp:LinkButton>
+                </span>
+                <span style="padding: 0px 5px 5px 5px;">
+                    <asp:LinkButton ID="lbtnDateFuture" runat="server" onclick="lbtnFuture_Click"></asp:LinkButton>
+                </span>
+            </div>
+            <div id="table" style="width:inherit; margin-left: auto; margin-right: auto; text-align:center;">
+                <asp:Label ID="lblTable" runat="server" onprerender="generateTable"></asp:Label>
+            </div>
+            <div id="pageselectors" style="width:inherit; margin-left: auto; margin-right: auto; text-align:center;">
+                <span style="padding: 0px 5px 5px 5px;">
+                    <asp:LinkButton ID="lbtnPagePrevious" runat="server" 
+                    onclick="lbtnPagePrevious_Click"></asp:LinkButton>
+                </span>
+                <span style="padding: 0px 5px 5px 5px;">
+                    <asp:LinkButton ID="lbtnPageNext" runat="server" 
+                    onclick="lbtnPageNext_Click"></asp:LinkButton>
+                </span>
+            </div>
+        </td>
+    </tr>
+    <tr><!-- New Page Code Starts Here -->
+        <td colspan="2">
+            <span style="padding: 2px 0px 5px 0px;">
+                <asp:Literal ID="lblPageStatus" runat="server">Add Current Reservation:</asp:Literal>
+            </span>
+            <div id="highlightrooms">
+                <span style="padding: 5px 5px 0px 0px">Highlight rooms of type:</span>
+                <asp:DropDownList ID="ddlRoomTypes" runat="server" 
+                    OnDataBound="ddlRoomTypes_OnDataBind" DataSourceID="sqlRoomTypes" 
+                    DataTextField="RoomType" DataValueField="RoomType" AutoPostBack="True">
+                </asp:DropDownList>
+                <!-- I know this isn't the best choice for a data source, but I was having
+                issues using the object data source -->
+                <asp:SqlDataSource ID="sqlRoomTypes" runat="server" 
+                    ConnectionString="<%$ ConnectionStrings:TreasureLandDB %>" 
+                    SelectCommand="SELECT RoomType, HotelRoomTypeID FROM HotelRoomType">
+                </asp:SqlDataSource>
+            </div>
+        </td>
+    </tr>
+    <tr>
+        <td colspan="2">
+            
+            <asp:MultiView ID="mvRooms" runat="server" ActiveViewIndex="1">
+                <asp:View ID="viewUpdateRoom" runat="server">
+                    <div id="UpdateReservation">
+                        <span style="padding: 5px 5px 0px 0px">Reservation #:</span>
+                        <asp:TextBox ID="txtReservationNumber" runat="server" ControlToValidate="txtReservationNumber"></asp:TextBox>
+                        <asp:RegularExpressionValidator ID="revReservationNumber" runat="server" 
+                            ForeColor="Red" ValidationExpression="^\d+$" 
+                            ControlToValidate="txtReservationNumber">Reservation # must be a number!</asp:RegularExpressionValidator>
+                        <br />
+                        <span style="padding: 5px 5px 0px 0px">Room #:</span>
+                        <asp:TextBox ID="txtRoomNumberUpdate" runat="server"></asp:TextBox>
+                        <span style="padding: 5px 5px 0px 0px"></span>
+                        <asp:Button ID="btnUpdateReservation" runat="server" Text="Update Reservation" 
+                            onclick="btnUpdateReservation_Click" />
+                        <br />
+                        <asp:Label ID="lblUpdateError" runat="server" ForeColor="Red"></asp:Label>
+                    </div>
+                </asp:View>
+                <asp:View ID="viewSelectRoom" runat="server">
+                    <div id="AddReservation">
+                        <span style="padding: 5px 5px 0px 0px">Room #:</span>
+                        <asp:TextBox ID="txtRoomNumberSelect" runat="server"></asp:TextBox>
+                        <span style="padding: 5px 15px 0px 0px"></span>
+                        <asp:Button ID="btnSelectRoom" runat="server" Text="Select Room" 
+                            onclick="btnSelectRoom_Click" />
+                        <asp:Label ID="lblSelectError" runat="server" ForeColor="Red"></asp:Label>
+                    </div>
+                </asp:View>
+            </asp:MultiView>
+            <div id="BackButton" style="float:right;">
+                <asp:Button ID="btnBack" runat="server" Text="Back" PostBackUrl="~/Clerk/CreateReservation.aspx" />
+            </div>
+        </td>
+    </tr>
+</table>
+    <script type="text/javascript">
+    <!--
+        var oldColor;
+
+        function select(room) {
+            var colZero = document.getElementById("row" + room);
+            oldColor = colZero.style.backgroundColor;
+            colZero.style.backgroundColor = "#7CC4FF";
+
+            for (var i = 0; i < 7; i++) {
+                var id = document.getElementById("room" + room + "col" + i + "a");
+                if (id != null)
+                    id.style.color = "#008CFF";
+                id = document.getElementById("room" + room + "col" + i + "b");
+                if (id != null)
+                    id.style.color = "#008CFF";
+            }
+        }
+
+        function deselect(room) {
+            document.getElementById("row" + room).style.backgroundColor = oldColor;
+
+            for (var i = 0; i < 7; i++) {
+                var id = document.getElementById("room" + room + "col" + i + "a");
+                if (id != null)
+                    id.style.color = "#696969";
+                id = document.getElementById("room" + room + "col" + i + "b");
+                if (id != null)
+                    id.style.color = "#696969";
+            }
+        }
+    -->
+    </script>
+</asp:Content>
---
+++ trunk/wsu-treasureland-hotel/TreasureLand/TreasureLand/Clerk/SelectRoom.aspx.cs
@@ -0,0 +1,236 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using TreasureLand.App_Code;
+
+namespace TreasureLand.Clerk
+{
+    public partial class SelectRoom : System.Web.UI.Page
+    {
+        private GridRangeView cHome = new GridRangeView();
+        private bool requiresUpdate = true;
+        private ListItem selectRoomType = new ListItem("-- Select a room type --", "None");
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (IsPostBack)
+                return;
+            
+            //SESSION key = "Room". ReservationID
+            //Assuming that if a room is passed in the clerk is selecting a room
+            //for a reservation. If no room value exists, then the clerk is updating
+            //an existing reservation by removing it to a different room
+            if (Session["Room"] == null)
+            {
+                mvRooms.ActiveViewIndex = 0; //Update room display
+                lblPageStatus.Text = "Move Reservation to a Different Room:";
+            }
+            else //Adding a room
+            {
+                mvRooms.ActiveViewIndex = 1; //Select room display
+                lblPageStatus.Text = "Add Current Reservation:";
+            }
+        }
+
+        /// <summary>
+        /// Adds a default "-- Select Room Type --" to the ddlRoomTypes
+        /// drop down list
+        /// </summary>
+        /// <param name="source"></param>
+        /// <param name="e"></param>
+        public void ddlRoomTypes_OnDataBind(object source, EventArgs e)
+        {
+            ddlRoomTypes.Items.Insert(0, selectRoomType);
+        }
+
+        /// <summary>
+        /// Generates the GridRangeView object before the HTML page is
+        /// rendered.
+        /// </summary>
+        /// <param name="sender">lbtnTable</param>
+        /// <param name="e"></param>
+        protected void generateTable(object sender, EventArgs e)
+        {
+            if (requiresUpdate)
+                cHome.update();
+            lblTable.Text = cHome.generateHTMLTablev3(true, ddlRoomTypes.SelectedValue);
+
+            //Generate label information
+            lbtnDatePrevious.Text = "Previous " + GridRangeView.DaysDisplayed + " Days";
+            lbtnDateFuture.Text = "Next " + GridRangeView.DaysDisplayed + " Days";
+            lbtnPageNext.Text = "Next " + GridRangeView.PageSize + " Rooms";
+            lbtnPagePrevious.Text = "Previous " + GridRangeView.PageSize + " Rooms";
+
+            if (GridRangeView.RoomIndex < GridRangeView.PageSize)
+                lbtnPagePrevious.Enabled = false;
+            else
+                lbtnPagePrevious.Enabled = true;
+
+            if (GridRangeView.RoomIndex + GridRangeView.PageSize >= GridRangeView.MaxRooms)
+                lbtnPageNext.Enabled = false;
+            else
+                lbtnPagePrevious.Enabled = true;
+
+            requiresUpdate = false;
+        }
+
+        /// <summary>
+        /// Tests a specific ReservationID against all records except itself
+        /// to test against existing date ranges to make sure a record will
+        /// not overlap a currently existing one
+        /// </summary>
+        /// <param name="ReservationID">ID of the reservation</param>
+        /// <param name="RoomNumber">Intended/New RoomNumber of the reservation</param>
+        /// <returns>Null if no collision. Else it returns errors</returns>
+        private string testDateRangeCollision(int ReservationID, string RoomNumber)
+        {
+            return cHome.testDateRangeCollision(ReservationID, RoomNumber);
+        }
+
+        /// <summary>
+        /// Decrements the current date by the DaysDisplayed
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public void lbtnPrevious_Click(object sender, EventArgs e)
+        {
+            GridRangeView.current = GridRangeView.current.AddDays(-GridRangeView.DaysDisplayed);
+            requiresUpdate = true;
+        }
+
+        /// <summary>
+        /// Increments the current date by the DaysDisplayed
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public void lbtnFuture_Click(object sender, EventArgs e)
+        {
+            GridRangeView.current = GridRangeView.current.AddDays(GridRangeView.DaysDisplayed);
+            requiresUpdate = true;
+        }
+
+        /// <summary>
+        /// Sets the current date to the current time
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public void lbtnToday_Click(object sender, EventArgs e)
+        {
+            GridRangeView.current = DateTime.Now.Date;
+            calDatePicker.SelectedDate = GridRangeView.current;
+            requiresUpdate = true;
+        }
+
+        /// <summary>
+        /// Sets the paging controls back by the number of room defined in the GridRangeView class
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        protected void lbtnPagePrevious_Click(object sender, EventArgs e)
+        {
+            GridRangeView.RoomIndex -= GridRangeView.PageSize;
+            requiresUpdate = true;
+        }
+
+        /// <summary>
+        /// Sets the paging controls forward by the number of room defined in 
+        /// the GridRangeView class
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        protected void lbtnPageNext_Click(object sender, EventArgs e)
+        {
+            GridRangeView.RoomIndex += GridRangeView.PageSize;
+            requiresUpdate = true;
+        }
+
+        /// <summary>
+        /// Sets the GridRangeView table to display a specific date
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        protected void calDatePicker_SelectionChanged(object sender, EventArgs e)
+        {
+            GridRangeView.current = calDatePicker.SelectedDate;
+            requiresUpdate = true;
+        }
+
+        /// <summary>
+        /// Updates a currently existing reservation by moving it to a new room
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        protected void btnUpdateReservation_Click(object sender, EventArgs e)
+        {
+            if (txtReservationNumber.Text == String.Empty)
+            {
+                lblUpdateError.Text = "Reservation number was not specified";
+                return;
+            }
+            if (txtRoomNumberUpdate.Text == String.Empty)
+            {
+                lblUpdateError.Text = "Room number was not specified";
+                return;
+            }
+
+            int ReservationID = Int32.Parse(txtReservationNumber.Text);
+            string errors = testDateRangeCollision(ReservationID, txtRoomNumberUpdate.Text);
+            if (errors != null)
+            {
+                lblUpdateError.Text = errors;
+                return;
+            }
+            lblUpdateError.Text = String.Empty;
+
+            //The database can now be safely updated
+            int rows = RoomDB.updateRoom(ReservationID, RoomDB.getRoomId(txtRoomNumberUpdate.Text));
+
+            if (rows <= 0)
+                lblUpdateError.Text = "There was a problem updating the room number.";
+            else
+                lblUpdateError.Text = "Room was successfully changed.";
+
+            requiresUpdate = true;
+        }
+
+        /// <summary>
+        /// Assuming the reservation is already in the database (but just lacks 
+        /// a definitive room number), the user will select a room number
+        /// and then that room number will be passed back in a Session
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        protected void btnSelectRoom_Click(object sender, EventArgs e)
+        {
+            if (txtRoomNumberSelect.Text == String.Empty)
+            {
+                lblUpdateError.Text = "Room number was not specified";
+                return;
+            }
+            
+            int ReservationID = Int32.Parse(Session["Room"].ToString());
+            string errors = testDateRangeCollision(ReservationID, txtRoomNumberSelect.Text);
+            if (errors != null)
+            {
+                lblSelectError.Text = errors;
+                return;
+            }
+            lblSelectError.Text = String.Empty;
+
+            //Get the room ID
+            int RoomID = RoomDB.getRoomId(txtRoomNumberSelect.Text);
+
+            if (Session["RoomID"] == null)
+                Session.Add("RoomID", RoomID);
+            else
+                Session["RoomID"] = RoomID;
+
+            requiresUpdate = true;
+            Session.Remove("Room");
+            Response.Redirect("~/Clerk/CreateReservation.aspx");
+        }
+    }
+}
