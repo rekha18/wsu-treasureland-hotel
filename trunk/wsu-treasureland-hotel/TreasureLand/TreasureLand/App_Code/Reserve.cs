2b24aed7101347e6e9fe010b2c5f948a0a0608a8
littlevalder@gmail.com <littlevalder@gmail.com@4b4c9caf-8d78-7f06-f573-cc5358e76b87>
2011-11-05 14:12:59 +0000

    Added reserve class updated few things on create reservation

---
+++ trunk/wsu-treasureland-hotel/TreasureLand/TreasureLand/App_Code/Reserve.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TreasureLand.App_Code
+{
+
+    public class Reserve
+    {
+        public Int16 roomID;
+        public string reserveDate;
+        public int daysStaying;
+        public int view;
+    }
+}
