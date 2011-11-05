593560ccdbcc0dd17386c3cfe5118b66340d6339
littlevalder@gmail.com <littlevalder@gmail.com@4b4c9caf-8d78-7f06-f573-cc5358e76b87>
2011-11-05 22:24:01 +0000

    Started Work on Update Reservation Page

--- trunk/wsu-treasureland-hotel/TreasureLand/TreasureLand/Clerk/UpdateReservation.aspx.cs
+++ trunk/wsu-treasureland-hotel/TreasureLand/TreasureLand/Clerk/UpdateReservation.aspx.cs
@@ -1,119 +1,50 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Data;
+using System.Data.SqlClient;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-using TreasureLand.App_Code;
+using TreasureLand.DBM;
 
 namespace TreasureLand.Clerk
 {
-    public partial class WebForm6 : System.Web.UI.Page
+    public partial class UpdateReservation : System.Web.UI.Page
     {
-        private GridRangeView cHome = new GridRangeView();
-        private bool requiresUpdate = true;
+
 
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            
         }
 
-        /// <summary>
-        /// Generates the GridRangeView object before the HTML page is
-        /// rendered.
-        /// </summary>
-        /// <param name="sender">lbtnTable</param>
-        /// <param name="e"></param>
-        protected void generateTable(object sender, EventArgs e)
+        protected void btnLocateReservation_Click(object sender, EventArgs e)
         {
-            if (requiresUpdate)
-                cHome.update();
-            lblTable.Text = cHome.generateHTMLTablev3(true, "None");
-
-            //Generate label information
-            lbtnDatePrevious.Text = "Previous " + GridRangeView.DaysDisplayed + " Days";
-            lbtnDateFuture.Text = "Next " + GridRangeView.DaysDisplayed + " Days";
-            lbtnPageNext.Text = "Next " + GridRangeView.PageSize + " Rooms";
-            lbtnPagePrevious.Text = "Previous " + GridRangeView.PageSize + " Rooms";
-
-            if (GridRangeView.RoomIndex < GridRangeView.PageSize)
-                lbtnPagePrevious.Enabled = false;
-            else
-                lbtnPagePrevious.Enabled = true;
-
-            if (GridRangeView.RoomIndex + GridRangeView.PageSize >= GridRangeView.MaxRooms)
-                lbtnPageNext.Enabled = false;
-            else
-                lbtnPagePrevious.Enabled = true;
-
-            requiresUpdate = false;
+            //Locates guest in database based on the values given
+            TreasureLandDataClassesDataContext db = new TreasureLandDataClassesDataContext();
+            var guest = from g in db.Guests
+                        join r in db.Reservations
+                        on g.GuestID equals r.GuestID
+                        where r.ReservationID.ToString() == txtResNumber.Text || g.GuestFirstName == txtFirstName.Text || g.GuestSurName == txtSurName.Text || g.GuestPhone == txtPhone.Text
+                        select new {r.ReservationID, g.GuestFirstName, g.GuestSurName, g.GuestPhone};
+            gvGuest.DataSource = guest.ToList();
+            gvGuest.DataBind();
         }
 
-        /// <summary>
-        /// Decrements the current date by the DaysDisplayed
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        public void lbtnPrevious_Click(object sender, EventArgs e)
+        protected void btnSelectReservation_Click(object sender, EventArgs e)
         {
-            GridRangeView.current = GridRangeView.current.AddDays(-GridRangeView.DaysDisplayed);
-            requiresUpdate = true;
+            lblReservationNumber.Text = gvGuest.SelectedRow.Cells[0].Text;
         }
 
-        /// <summary>
-        /// Increments the current date by the DaysDisplayed
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        public void lbtnFuture_Click(object sender, EventArgs e)
+
+        protected void gvGuest_SelectedIndexChanged(object sender, EventArgs e)
         {
-            GridRangeView.current = GridRangeView.current.AddDays(GridRangeView.DaysDisplayed);
-            requiresUpdate = true;
+            btnSelectReservation.Enabled = true;
         }
 
-        /// <summary>
-        /// Sets the current date to the current time
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        public void lbtnToday_Click(object sender, EventArgs e)
-        {
-            GridRangeView.current = DateTime.Now.Date;
-            requiresUpdate = true;
-        }
+        
 
-        /// <summary>
-        /// Sets the paging controls back by the number of room defined in the GridRangeView class
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        protected void lbtnPagePrevious_Click(object sender, EventArgs e)
-        {
-            GridRangeView.RoomIndex -= GridRangeView.PageSize;
-            requiresUpdate = true;
-        }
-
-        /// <summary>
-        /// Sets the paging controls forward by the number of room defined in 
-        /// the GridRangeView class
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        protected void lbtnPageNext_Click(object sender, EventArgs e)
-        {
-            GridRangeView.RoomIndex += GridRangeView.PageSize;
-            requiresUpdate = true;
-        }
-
-        /// <summary>
-        /// Sets the GridRangeView table to display a specific date
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        protected void calDatePicker_SelectionChanged(object sender, EventArgs e)
-        {
-            GridRangeView.current = calDatePicker.SelectedDate;
-            requiresUpdate = true;
-        }
+        
     }
 }
