5582e34dad7dce29833a14b69adedcace3f9c85b
littlevalder <littlevalder@gmail.com>
2011-11-06 21:11:29 +0000

    add ability for master page buttons to clear sessions

--- wsu-treasureland-hotel/TreasureLand/TreasureLand/Clerk/ClerkMasterPage.master.cs
+++ wsu-treasureland-hotel/TreasureLand/TreasureLand/Clerk/ClerkMasterPage.master.cs
@@ -17,41 +17,49 @@ namespace TreasureLand.Clerk
         protected void btnHome_Click(object sender, EventArgs e)
         {
             Response.Redirect("ClerkDefault.aspx");
+            Session.Remove("Room");
         }
 
         protected void btnCheckIn_Click(object sender, EventArgs e)
         {
             Response.Redirect("LocateGuest.aspx");
+            Session.Remove("Room");
         }
 
         protected void Checkout_Click(object sender, EventArgs e)
         {
             Response.Redirect("LocateReservation.aspx");
+            Session.Remove("Room");
         }
 
         protected void btnUpdateGuestFolio_Click(object sender, EventArgs e)
         {
             Response.Redirect("UpdateGuestFolio.aspx");
+            Session.Remove("Room");
         }
 
         protected void btnCreateReservation_Click(object sender, EventArgs e)
         {
             Response.Redirect("CreateReservation.aspx");
+            Session.Remove("Room");
         }
 
         protected void btnUpdateReservation_Click(object sender, EventArgs e)
         {
             Response.Redirect("UpdateReservation.aspx");
+            Session.Remove("Room");
         }
 
         protected void btnViewGuest_Click(object sender, EventArgs e)
         {
             Response.Redirect("ViewGuests.aspx");
+            Session.Remove("Room");
         }
 
         protected void btnReports_Click(object sender, EventArgs e)
         {
             Response.Redirect("Reports.aspx");
+            Session.Remove("Room");
         }
     }
 }
