5b468f728c584ad60be5018d0f0b0348538931e4
jasonlnightengale <jasonlnightengale@gmail.com>
2011-11-08 01:10:26 +0000

    Guess I forgot to upload this file

---
+++ wsu-treasureland-hotel/TreasureLand/TreasureLand/App_Code/Discount.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TreasureLand.App_Code
+{
+    public class Discount
+    {
+        public int ID;
+        public string description;
+        public bool isPercent;
+        public double amountOfDiscount;
+        public Discount() { }
+        public Discount(int ID, string description, bool isPercent, double amountOfDiscount)
+        {
+            this.ID = ID;
+            this.description = description;
+            this.isPercent = isPercent;
+            this.amountOfDiscount = amountOfDiscount;
+        }
+
+        public override string ToString()
+        {
+            if (this.isPercent == true)
+            {
+                return description + " " + amountOfDiscount + "%";
+            }
+            else
+            return description + amountOfDiscount;
+        }
+
+    }
+}
