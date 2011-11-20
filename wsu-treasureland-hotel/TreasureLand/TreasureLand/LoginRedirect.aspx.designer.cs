b8a890bd5bf96255be0b0ee68cd1dbf37b17cb14
jimmywhip <jimmywhip@gmail.com>
2011-11-20 14:07:59 +0000

    Made it all nice and purdy

--- wsu-treasureland-hotel/TreasureLand/TreasureLand/LoginRedirect.aspx.designer.cs
+++ wsu-treasureland-hotel/TreasureLand/TreasureLand/LoginRedirect.aspx.designer.cs
@@ -13,6 +13,15 @@ namespace TreasureLand {
     public partial class LoginRedirect {
         
         /// <summary>
+        /// Panel1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Panel Panel1;
+        
+        /// <summary>
         /// btnAdmin control.
         /// </summary>
         /// <remarks>
