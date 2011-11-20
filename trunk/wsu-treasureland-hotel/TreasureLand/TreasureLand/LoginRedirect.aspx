e2eb7db629898ee648dfd3b58dd7554814168021
littlevalder@gmail.com <littlevalder@gmail.com@4b4c9caf-8d78-7f06-f573-cc5358e76b87>
2011-11-20 13:18:41 +0000

    Added a multiple role login redirect

---
+++ trunk/wsu-treasureland-hotel/TreasureLand/TreasureLand/LoginRedirect.aspx
@@ -0,0 +1,39 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="LoginRedirect.aspx.cs" Inherits="TreasureLand.LoginRedirect" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="HeadContent" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder_RoleMenu" runat="server">
+</asp:Content>
+<asp:Content ID="Content3" ContentPlaceHolderID="MainContent" runat="server">
+    <table style="width:100%;">
+    <tr>
+        <td>
+            <asp:Button ID="btnAdmin" runat="server" Text="Admin" Visible="False" 
+                onclick="btnAdmin_Click" />
+        </td>
+    </tr>
+    <tr>
+        <td>
+            <asp:Button ID="btnManager" runat="server" Text="Manager" Visible="False" 
+                onclick="btnManager_Click" />
+        </td>
+    </tr>
+    <tr>
+        <td>
+            <asp:Button ID="btnClerk" runat="server" Text="Clerk" Visible="False" 
+                onclick="btnClerk_Click" />
+        </td>
+    </tr>
+    <tr>
+        <td>
+            <asp:Button ID="btnHousekeeping" runat="server" Text="Housekeeping" 
+                Visible="False" onclick="btnHousekeeping_Click" />
+        </td>
+    </tr>
+    <tr>
+        <td>
+            <asp:Button ID="btnMaintenance" runat="server" Text="Maintenance" 
+                Visible="False" onclick="btnMaintenance_Click" />
+        </td>
+    </tr>
+</table>
+</asp:Content>
---
+++ trunk/wsu-treasureland-hotel/TreasureLand/TreasureLand/LoginRedirect.aspx.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using TreasureLand.App_Code;
+using System.Security;
+using System.Web.Security;
+using System.Collections;
+using System.Data;
+using System.Data.SqlClient;
+using TreasureLand.DBM;
+
+namespace TreasureLand
+{
+    public partial class LoginRedirect : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Roles.IsUserInRole("Admin"))
+            {
+                btnAdmin.Visible = true;
+            }
+            if (Roles.IsUserInRole("Manager"))
+            {
+                btnManager.Visible = true;
+            } 
+            if (Roles.IsUserInRole("Clerk"))
+            {
+                btnClerk.Visible = true;
+            }
+            if (Roles.IsUserInRole("Housekeeping"))
+            {
+                btnHousekeeping.Visible = true;
+            }
+            if (Roles.IsUserInRole("Maintenance"))
+            {
+                btnMaintenance.Visible = true;
+            }
+        }
+
+        protected void btnAdmin_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("~/Admin/AdminDefault.aspx");
+        }
+
+        protected void btnManager_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("~/Manager/ManagerDefault.aspx");
+        }
+
+        protected void btnClerk_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("~/Clerk/ClerkDefault.aspx");
+        }
+
+        protected void btnHousekeeping_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("~/Housekeeping/HousekeepingDefault.aspx");
+        }
+
+        protected void btnMaintenance_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("~/Maintenance/MaintenanceDefault.aspx");
+        }
+    }
+}
