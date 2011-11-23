cd30116f3e6f6535f35faf293ae91215da6c0781
jimmywhip <jimmywhip@gmail.com>
2011-11-23 23:00:03 +0000

    Validation now uses a summary to describe what the error is Fields that fail validation will not prevent navigation to other webpages

--- wsu-treasureland-hotel/TreasureLand/TreasureLand/Clerk/UpdateGuestFolio.aspx.designer.cs
+++ wsu-treasureland-hotel/TreasureLand/TreasureLand/Clerk/UpdateGuestFolio.aspx.designer.cs
@@ -265,6 +265,15 @@ namespace TreasureLand.Clerk {
         protected global::System.Web.UI.WebControls.TextBox txtComments;
         
         /// <summary>
+        /// vsSummary control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ValidationSummary vsSummary;
+        
+        /// <summary>
         /// lblError control.
         /// </summary>
         /// <remarks>
