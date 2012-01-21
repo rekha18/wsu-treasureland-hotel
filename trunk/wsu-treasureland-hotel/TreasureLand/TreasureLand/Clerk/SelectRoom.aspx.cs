8dcce8fa61b2e0cdba37fe94fb221382929c067c
jimmywhip@gmail.com <jimmywhip@gmail.com@4b4c9caf-8d78-7f06-f573-cc5358e76b87>
2012-01-21 15:55:30 +0000

    Removed table-click interface Added GridView that displays all open rooms for the date range provided
    Known bugs:
    When switching between room types, the clicked rooms remain in memory but do
    not correctly "check" themselves when returning to the room type they were
    initially checked on. Selecting or deselecting a box will update the page
    and it will show all previously checked rooms.

--- trunk/wsu-treasureland-hotel/TreasureLand/TreasureLand/Clerk/SelectRoom.aspx.cs
+++ trunk/wsu-treasureland-hotel/TreasureLand/TreasureLand/Clerk/SelectRoom.aspx.cs
@@ -10,330 +10,138 @@ namespace TreasureLand.Clerk
 {
     public partial class SelectRoom : System.Web.UI.Page
     {
-        private GridRangeView cHome;
-        private bool requiresUpdate = true;
-        private ListItem selectRoomType = new ListItem("-- Select a room type --", "None");
-        private static Reserve reserve;
-        //private static string CheckInDate;
-        //private static int Nights;
+        private struct RowInfo
+        {
+            public string RoomType;
+            public int RowIndex;
 
+            public RowInfo(string RoomType, int RowIndex)
+            {
+                this.RoomType = RoomType;
+                this.RowIndex = RowIndex;
+            }
+        }
+        
+        /// <summary>
+        /// Runs when the page loads. The main purpose is to read information
+        /// from the session array and set the literals to display the
+        /// information
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         protected void Page_Load(object sender, EventArgs e)
         {
-            if (Session["Date"] == null)
+            //On a fresh load, remove any previous RowInfo
+            if (!IsPostBack)
+                if(Session["RowInfo"] != null)
+                    Session.Remove("RowInfo");
+            
+            if (Session["RowInfo"] == null)
+                Session.Add("RowInfo", new Dictionary<int, RowInfo>());
+            if (Session["LastRoomType"] == null)
+                Session.Add("LastRoomType", ddlRoomTypes.SelectedValue);
+            
+            if (Session["StartDate"] == null)
+                Session.Add("StartDate", DateTime.Now);
+            if (Session["Nights"] == null)
+                Session.Add("Nights", 3);
+            
+            lblStartDate.Text = Session["StartDate"].ToString();
+            lNights.Text = Session["Nights"].ToString();
+
+            checkSelectedRooms();
+        }
+
+        /// <summary>
+        /// Updates the page for information dependant on the total selected rooms
+        /// </summary>
+        /// <param name="roomsSelected">Total rooms that have been selected</param>
+        private void updatePageInfo(int roomsSelected)
+        {
+            if (roomsSelected > 0)
             {
-                Session.Add("Date", DateTime.Now.Date);
-                Session.Add("RoomIndex", 0);
-                cHome = new GridRangeView((DateTime)Session["Date"], 0);
+                lblTotalRooms.Text = roomsSelected.ToString();
+                btnSelect.Enabled = true;
             }
             else
-                cHome = new GridRangeView((DateTime)Session["Date"], (int)Session["RoomIndex"]);
-            
-            if (IsPostBack)
-                return;
-            //else
-            //{
-                //Session.Add("CheckInDate", "10/31/2011");
-                //Session.Add("Nights", 5);
-            //}
-            
-            //SESSION key = "Room". ReservationID
-            //Assuming that if a room is passed in the clerk is selecting a room
-            //for a reservation. If no room value exists, then the clerk is updating
-            //an existing reservation by removing it to a different room
-            reserve = (Reserve)Session["Room"];
-            if (reserve == null || reserve.view == 0)
             {
-                mvRooms.ActiveViewIndex = 0; //Update room display
-                lblPageStatus.Text = "Move Reservation to a Different Room:";
-                calDatePicker.Visible = true;
+                lblTotalRooms.Text = "0";
+                btnSelect.Enabled = false;
+            }
+        }
 
-                Row res = null;
-                if(reserve != null)
-                    if (reserve.reservationID != 0)
-                    {
-                        res = cHome.getRowInformation(reserve.reservationID);
+        /// <summary>
+        /// Re-enables any selected rooms after the room type was changed
+        /// </summary>
+        private void checkSelectedRooms()
+        {
+            Dictionary<int, RowInfo> rowInfo = (Dictionary<int, RowInfo>)Session["RowInfo"];
 
-                        if (res != null)
-                        {
-                            cHome.current = res.Begin;
-                            Session["Date"] = cHome.current;
-                            lGuestName.Text = res.GuestName;
-                            lReservationID.Text = res.ReservationID + String.Empty;
-                            lDetailID.Text = res.ReservationDetailID + String.Empty;
-                        }
-                    }
+            foreach (KeyValuePair<int, RowInfo> kvp in rowInfo)
+            {
+                RowInfo ri = kvp.Value;
 
-                lDID.Text = "Detail ID:";
-                lResID.Text = "Reservation ID:";
-                lname.Text = "Guest Name:";
-                loriginal.Text = "Original Info:";
-                lblNewTableInfo.Text = "<table>" +
-                    "<tr><th colspan='2'>Selected Info:</th></tr>" +
-                    "<tr><td>Name: </td><td id='idName'></td></tr>" +
-                    "<tr><td>Reservation ID: </td><td id='idReservationID2'></td></tr>" +
-                    "<tr><td>Detail ID: </td><td id='idDetailID2'></td></tr>" +
-                    "</table>";
-                if (res != null)
+                //The current room type is also this room's type
+                if(ri.RoomType == ddlRoomTypes.SelectedValue)
                 {
-                    txtReservationNumber.Text = res.ReservationDetailID + String.Empty;
-                    txtReservationDate.Text = res.Begin.ToString("dd/MM/yyyy");
-                    ddlNightsStayed.SelectedIndex = (res.End - res.Begin).Days - 1;
+                    CheckBox cb = (CheckBox)gvOpenRooms.Rows[ri.RowIndex].FindControl("cbSelected");
+                    cb.Checked = true;
                 }
             }
-            else //Adding a room
-            {
-                mvRooms.ActiveViewIndex = 1; //Select room display
-                lblPageStatus.Text = "Add Current Reservation:";
-                cHome.current = DateTime.Parse(reserve.reserveDate);
-                calDatePicker.SelectedDate = cHome.current;
-                Session["Date"] = cHome.current;
-                calDatePicker.Visible = false;
-
-                lblDateBegin.Text = reserve.reserveDate;
-                lblDateEnd.Text = DateTime.Parse(reserve.reserveDate).AddDays(reserve.daysStaying).ToString("dd/MM/yyyy");
-                //CheckInDate = Session["CheckInDate"].ToString();
-                //Nights = Int32.Parse(Session["Nights"].ToString());
-                //Session.Remove("CheckInDate");
-                //Session.Remove("Nights");
-                //reserve = (Reserve)Session["Room"];
-                //Session.Remove("Remove");
-            }
         }
 
         /// <summary>
-        /// Adds a default "-- Select Room Type --" to the ddlRoomTypes
-        /// drop down list
-        /// </summary>
-        /// <param name="source"></param>
-        /// <param name="e"></param>
-        public void ddlRoomTypes_OnDataBind(object source, EventArgs e)
-        {
-            ddlRoomTypes.Items.Insert(0, selectRoomType);
-        }
-
-        /// <summary>
-        /// Generates the GridRangeView object before the HTML page is
-        /// rendered.
-        /// </summary>
-        /// <param name="sender">lbtnTable</param>
-        /// <param name="e"></param>
-        protected void generateTable(object sender, EventArgs e)
-        {
-            if (requiresUpdate)
-                cHome.update();
-            lblTable.Text = cHome.generateHTMLTablev3(true, ddlRoomTypes.SelectedValue);
-
-            //Generate label information
-            lbtnDatePrevious.Text = "Previous " + GridRangeView.DaysDisplayed + " Days";
-            lbtnDateFuture.Text = "Next " + GridRangeView.DaysDisplayed + " Days";
-            lbtnPageNext.Text = "Next " + GridRangeView.PageSize + " Rooms";
-            lbtnPagePrevious.Text = "Previous " + GridRangeView.PageSize + " Rooms";
-
-            if (cHome.RoomIndex < GridRangeView.PageSize)
-                lbtnPagePrevious.Enabled = false;
-            else
-                lbtnPagePrevious.Enabled = true;
-
-            if (cHome.RoomIndex + GridRangeView.PageSize >= GridRangeView.MaxRooms)
-                lbtnPageNext.Enabled = false;
-            else
-                lbtnPageNext.Enabled = true;
-
-            requiresUpdate = false;
-        }
-
-        /// <summary>
-        /// Tests a specific ReservationID against all records except itself
-        /// to test against existing date ranges to make sure a record will
-        /// not overlap a currently existing one
-        /// </summary>
-        /// <param name="ReservationID">ID of the reservation</param>
-        /// <param name="RoomNumber">Intended/New RoomNumber of the reservation</param>
-        /// <returns>Null if no collision. Else it returns errors</returns>
-        private string testDateRangeCollision(int ReservationID, string RoomNumber)
-        {
-            return cHome.testDateRangeCollision(ReservationID, RoomNumber);
-        }
-
-        /// <summary>
-        /// Decrements the current date by the DaysDisplayed
+        /// 
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
-        public void lbtnPrevious_Click(object sender, EventArgs e)
+        protected void btnSelect_Click(object sender, EventArgs e)
         {
-            cHome.current = cHome.current.AddDays(-GridRangeView.DaysDisplayed);
-            Session["Date"] = cHome.current;
-            requiresUpdate = true;
+
         }
 
         /// <summary>
-        /// Increments the current date by the DaysDisplayed
+        /// Fires when a checkbox in the selected rooms list is changed
         /// </summary>
-        /// <param name="sender"></param>
+        /// <param name="sender">CheckBox that was toggled</param>
         /// <param name="e"></param>
-        public void lbtnFuture_Click(object sender, EventArgs e)
+        protected void cbSelected_CheckedChanged(object sender, EventArgs e)
         {
-            cHome.current = cHome.current.AddDays(GridRangeView.DaysDisplayed);
-            Session["Date"] = cHome.current;
-            requiresUpdate = true;
-        }
-
-        /// <summary>
-        /// Sets the current date to the current time
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        public void lbtnToday_Click(object sender, EventArgs e)
-        {
-            cHome.current = DateTime.Now.Date;
-            calDatePicker.SelectedDate = cHome.current;
-            Session["Date"] = cHome.current;
-            requiresUpdate = true;
-        }
-
-        /// <summary>
-        /// Sets the paging controls back by the number of room defined in the GridRangeView class
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        protected void lbtnPagePrevious_Click(object sender, EventArgs e)
-        {
-            if (cHome.RoomIndex >= GridRangeView.PageSize)
-            {
-                cHome.RoomIndex -= GridRangeView.PageSize;
-                Session["RoomIndex"] = cHome.RoomIndex;
-                lbtnPagePrevious.Focus();
-                requiresUpdate = true;
-            }
-        }
-
-        /// <summary>
-        /// Sets the paging controls forward by the number of room defined in 
-        /// the GridRangeView class
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        protected void lbtnPageNext_Click(object sender, EventArgs e)
-        {
-            if (cHome.RoomIndex + GridRangeView.PageSize < GridRangeView.MaxRooms)
-            {
-                cHome.RoomIndex += GridRangeView.PageSize;
-                Session["RoomIndex"] = cHome.RoomIndex;
-                lbtnPageNext.Focus();
-                requiresUpdate = true;
-            }
-        }
-
-        /// <summary>
-        /// Sets the GridRangeView table to display a specific date
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        protected void calDatePicker_SelectionChanged(object sender, EventArgs e)
-        {
-            cHome.current = calDatePicker.SelectedDate;
-            Session["Date"] = cHome.current;
-            requiresUpdate = true;
-        }
-
-        /// <summary>
-        /// Updates a currently existing reservation by moving it to a new room
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        protected void btnUpdateReservation_Click(object sender, EventArgs e)
-        {
-            if (txtReservationNumber.Text == String.Empty)
-            {
-                lblUpdateError.Text = "Reservation number was not specified";
-                return;
-            }
-            if (txtRoomNumberUpdate.Text == String.Empty)
-            {
-                lblUpdateError.Text = "Room number was not specified";
-                return;
-            }
-
-            int ReservationDetailID = Int32.Parse(txtReservationNumber.Text);
-            string errors = testDateRangeCollision(ReservationDetailID, txtRoomNumberUpdate.Text);
-            int ReservationID = cHome.getReservationNumber(ReservationDetailID);
-            if (errors != null)
-            {
-                lblUpdateError.Text = errors;
-                return;
-            }
-            lblUpdateError.Text = String.Empty;
-
-            DateTime resDate = DateTime.Parse(txtReservationDate.Text);
-            //The database can now be safely updated
-            int rows = RoomDB.updateRoom(ReservationID, ReservationDetailID, RoomDB.getRoomId(txtRoomNumberUpdate.Text),
-                resDate, Int32.Parse(ddlNightsStayed.Items[ddlNightsStayed.SelectedIndex].Value));
-
-            if (rows <= 0)
-                lblUpdateError.Text = "There was a problem updating the room number.";
-            else
-                lblUpdateError.Text = "Room was successfully changed.";
-
-            cHome.current = resDate;
-            Session["Date"] = resDate;
-            requiresUpdate = true;
-        }
-
-        /// <summary>
-        /// Assuming the reservation is already in the database (but just lacks 
-        /// a definitive room number), the user will select a room number
-        /// and then that room number will be passed back in a Session
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        protected void btnSelectRoom_Click(object sender, EventArgs e)
-        {
-            if (txtRoomNumberSelect.Text == String.Empty)
-            {
-                lblUpdateError.Text = "Room number was not specified";
-                return;
-            }
+            CheckBox cb = (CheckBox)sender;
+            int roomID = 0, rowID = 0;
+            Dictionary<int, RowInfo> rowInfo = (Dictionary<int, RowInfo>)Session["RowInfo"];
             
-            //int ReservationID = Int32.Parse(Session["Room"].ToString());
-            string errors = cHome.testDateRangeCollision(reserve.reserveDate, reserve.daysStaying, txtRoomNumberSelect.Text);
-            if (errors != null)
+            //Determine the exact RoomID of the checkbox
+            int rows = 0;
+            foreach (GridViewRow r in gvOpenRooms.Rows)
             {
-                lblSelectError.Text = errors;
-                return;
+                CheckBox temp = ((CheckBox)r.FindControl("cbSelected"));
+
+                //If the checkboxes have the same address
+                if(temp == cb)
+                {
+                    roomID = Convert.ToInt32(r.Cells[0].Text);
+                    rowID = rows;
+                    break;
+                }
+                rows++;
             }
-            lblSelectError.Text = String.Empty;
 
-            //Get the room ID
-            reserve.roomID = (short)RoomDB.getRoomId(txtRoomNumberSelect.Text);
-            reserve.view = 2;
+            //If the CheckBox hasn't been checked yet
+            if (!rowInfo.ContainsKey(roomID))
+            {
+                //Add it to the linked list
+                rowInfo.Add(roomID, new RowInfo(ddlRoomTypes.SelectedValue, rowID));
+                cb.Focus();
+            }
+            else
+            {
+                //Remove it from the linked list
+                rowInfo.Remove(roomID);
+                cb.Checked = false;
+            }
 
-            Session["Date"] = DateTime.Parse(reserve.reserveDate);
-            requiresUpdate = true;
-            Response.Redirect("~/Clerk/CreateReservation.aspx");
-        }
-
-        /// <summary>
-        /// Simply sets focus to the ddlRoomTypes control
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        protected void ddlRoomTypes_onSelectedIndexChanged(object sender, EventArgs e)
-        {
-            ddlRoomTypes.Focus();
-        }
-
-        /// <summary>
-        /// Modifies the internal reserve object so that the update view is not mistakenly
-        /// returned to when selecting a room
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        protected void btnBackToUpdateReservation_Click(object sender, EventArgs e)
-        {
-            reserve = reserve.Clone(); //Safely clone the object
-            reserve.view = 1;
-            Response.Redirect("~/Clerk/UpdateReservation.aspx");
+            updatePageInfo(rowInfo.Count);
         }
     }
 }
