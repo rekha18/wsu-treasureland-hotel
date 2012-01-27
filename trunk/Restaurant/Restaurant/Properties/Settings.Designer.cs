99a972ff34fccff4e3f9ad0b0e172b09af2dabb7
jasonlnightengale@gmail.com <jasonlnightengale@gmail.com@4b4c9caf-8d78-7f06-f573-cc5358e76b87>
2012-01-27 03:45:41 +0000

    added linq class you should be able to program against the database now

--- trunk/Restaurant/Restaurant/Properties/Settings.Designer.cs
+++ trunk/Restaurant/Restaurant/Properties/Settings.Designer.cs
@@ -8,22 +8,29 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Restaurant.Properties
-{
-
-
+namespace Restaurant.Properties {
+    
+    
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "10.0.0.0")]
-    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase
-    {
-
+    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
+        
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
-
-        public static Settings Default
-        {
-            get
-            {
+        
+        public static Settings Default {
+            get {
                 return defaultInstance;
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=\"titan.cs.weber.edu, 10433\";Initial Catalog=TreasureLand;Persist Secu" +
+            "rity Info=True;User ID=4790Client;Password=CS4790")]
+        public string TreasureLandConnectionString {
+            get {
+                return ((string)(this["TreasureLandConnectionString"]));
             }
         }
     }
