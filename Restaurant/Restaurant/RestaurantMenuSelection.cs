8bfada353713555d10744d8d45f2cc43ec2f1e0e
matthewrice345 <matthewrice345@gmail.com>
2012-01-27 02:48:58 +0000

    Added in the RestaurantMenuSelection.cs and RoomSelection.cs Matthew Rice

---
+++ Restaurant/Restaurant/RestaurantMenuSelection.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Restaurant
+{
+    public partial class RestaurantMenuSelection : Form
+    {
+        public RestaurantMenuSelection()
+        {
+            InitializeComponent();
+        }
+
+        public RestaurantMenuSelection(int selectedRoom)
+        {
+            InitializeComponent();
+            lbl_selectedRoom.Text = "Selected Room: " + selectedRoom.ToString();
+        }
+    }
+}
