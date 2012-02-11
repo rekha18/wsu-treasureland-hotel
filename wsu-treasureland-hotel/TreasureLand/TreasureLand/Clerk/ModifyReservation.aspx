9cddd64beb470d3661006c2d69f3892f33084adb
jimmywhip <jimmywhip@gmail.com>
2012-02-11 02:50:38 +0000

    Replaces the removed View Panels from the old SelectRoom.

---
+++ wsu-treasureland-hotel/TreasureLand/TreasureLand/Clerk/ModifyReservation.aspx
@@ -0,0 +1,59 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Clerk/ClerkMasterPage.master" AutoEventWireup="true" CodeBehind="ModifyReservation.aspx.cs" Inherits="TreasureLand.Clerk.ModifyReservation" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <p>Please select a room to change the currently chosen room to.<br /></p>
+    <p>Select a room type: 
+        <asp:DropDownList ID="ddlRoomTypes" runat="server" AutoPostBack="True" 
+            DataSourceID="ldsRoomTypes" DataTextField="RoomType" 
+            DataValueField="HotelRoomTypeID" 
+            onselectedindexchanged="ddlRoomTypes_SelectedIndexChanged">
+        </asp:DropDownList>
+        <asp:LinqDataSource ID="ldsRoomTypes" runat="server" 
+            ContextTypeName="TreasureLand.DBM.TreasureLandDataClassesDataContext" 
+            EntityTypeName="" Select="new (RoomType, HotelRoomTypeID)" 
+            TableName="HotelRoomTypes">
+        </asp:LinqDataSource>
+    </p>
+    <asp:GridView ID="gvOpenRooms" runat="server" AutoGenerateColumns="False" 
+        DataSourceID="sdsOpenRooms" AllowPaging="True" 
+        onpageindexchanged="gvOpenRooms_PageIndexChanged" 
+        onselectedindexchanged="gvOpenRooms_SelectedIndexChanged">
+        <Columns>
+            <asp:BoundField DataField="RoomID" HeaderText="Room ID" 
+                SortExpression="RoomID" />
+            <asp:BoundField DataField="RoomNumbers" HeaderText="Room Number" 
+                SortExpression="RoomNumbers" />
+            <asp:CommandField ButtonType="Button" ShowSelectButton="True" />
+        </Columns>
+        <SelectedRowStyle BackColor="Yellow" />
+    </asp:GridView>
+    <!-- A SQL data source is necessary here due to the complex operation of comparing
+    all records to ensure that a room is open for the specified date range -->
+    <asp:SqlDataSource ID="sdsOpenRooms" runat="server" 
+        ConnectionString="<%$ ConnectionStrings:TreasurelandDB %>" SelectCommand="SELECT RoomID, RoomNumbers FROM Room
+   WHERE RoomID != 
+   (
+      SELECT r.RoomID FROM Room r
+         INNER JOIN HotelRoomType hrt ON hrt.HotelRoomTypeID = r.HotelRoomTypeID 
+         INNER JOIN ReservationDetail rd ON rd.RoomID = r.RoomID 
+         INNER JOIN Reservation res ON res.ReservationID = rd.ReservationID
+         WHERE (@StartDate &lt;= (DATEADD(day, rd.Nights, res.ReservationDate)) AND
+               (DATEADD(day, @Nights, @StartDate)) &gt;= res.ReservationDate )
+   )
+   AND HotelRoomTypeID = @HotelRoomType
+   AND RoomStatus != 'M'
+   ORDER BY RoomID">
+        <SelectParameters>
+            <asp:SessionParameter Name="StartDate" SessionField="StartDate" />
+            <asp:SessionParameter Name="Nights" SessionField="Nights" />
+            <asp:ControlParameter ControlID="ddlRoomTypes" Name="HotelRoomType" 
+                PropertyName="SelectedValue" />
+        </SelectParameters>
+    </asp:SqlDataSource>
+    <asp:CheckBox ID="cbRemoveDiscount" runat="server" Text="Check if guest requested the room change." />
+    <p style="color:Red; margin: 0px; padding: 2px;">Checking will remove discounts and apply the standard room rate.</p>
+    <br />
+    <span style="margin: 0 0 0 300px">
+        <asp:Button ID="btnSelect" runat="server" Text="Change Room" 
+        Enabled="false" onclick="btnSelect_Click" 
+        PostBackUrl="~/Clerk/UpdateReservation.aspx" /></span>
+</asp:Content>
---
+++ wsu-treasureland-hotel/TreasureLand/TreasureLand/Clerk/ModifyReservation.aspx.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Configuration;
+using TreasureLand.App_Code;
+
+namespace TreasureLand.Clerk
+{
+    public partial class ModifyReservation : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Reserve res = (Reserve)Session["Room"];
+
+            if (Session["StartDate"] == null)
+                Session.Add("StartDate", res.reserveDate);
+            else
+                Session["StartDate"] = res.reserveDate;
+
+            if (Session["Nights"] == null)
+                Session.Add("Nights", res.daysStaying);
+            else
+                Session["Nights"] = res.daysStaying;
+
+            if (Session["ResDetailID"] == null)
+                Session.Add("ResDetailID", res.reservationDetailID);
+            else
+                Session["ResDetailID"] = res.reservationDetailID;
+        }
+
+        /// <summary>
+        /// Removes the selected value from the GridView to prevent a value from
+        /// being highlighted on a room type change
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        protected void ddlRoomTypes_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            gvOpenRooms.SelectedIndex = -1;
+            btnSelect.Enabled = false;
+        }
+
+        /// <summary>
+        /// Enables the "Change Room" button after something has been selected
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        protected void gvOpenRooms_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            btnSelect.Enabled = true;
+        }
+
+        /// <summary>
+        /// Deselects any selected rooms when changing the page index since the
+        /// chosen value's position is retained
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        protected void gvOpenRooms_PageIndexChanged(object sender, EventArgs e)
+        {
+            ddlRoomTypes_SelectedIndexChanged(sender, e);
+        }
+
+        /// <summary>
+        /// Updates the selected reservation to the specified room
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        protected void btnSelect_Click(object sender, EventArgs e)
+        {
+            int roomID = Convert.ToInt32(gvOpenRooms.SelectedRow.Cells[0].Text);
+            
+            if (cbRemoveDiscount.Checked)
+                updateRemoveDiscount(roomID);
+            else
+                updateKeepDiscount(roomID);
+
+            ((Reserve)Session["Room"]).roomID = (short)roomID;
+        }
+
+        /// <summary>
+        /// Updates the roomID of the specified ReservationDetailID.
+        /// The default rack rate for the room is applied, and the discount
+        /// is set to "No Discount"
+        /// </summary>
+        /// <param name="roomID">roomID to update</param>
+        private void updateRemoveDiscount(int roomID)
+        {
+            using (SqlConnection conn = new SqlConnection(
+                ConfigurationManager.ConnectionStrings["TreasureLandDB"].ConnectionString))
+            {
+                conn.Open();
+
+                string command = "UPDATE [ReservationDetail] SET [RoomID] = @RoomID, " +
+                                    "[DiscountID] = @DiscountID " +
+                                    "[QuotedRate] = ( " +
+                                       "SELECT [RoomTypeRackRate] FROM [HotelRoomType] " +
+                                       "WHERE [HotelRoomTypeID] = @HotelRoomTypeID " +
+                                       ") " +
+                                    "WHERE [ReservationDetailID] = @ReservationDetailID";
+                SqlCommand connCommand = new SqlCommand(command, conn);
+
+                connCommand.Parameters.AddWithValue("@RoomID", roomID);
+                connCommand.Parameters.AddWithValue("@ReservationDetailID", Convert.ToInt32(Session["ResDetailID"].ToString()));
+                connCommand.Parameters.AddWithValue("@DiscountID", 2); //2 is assumed to be the "No Discount"
+                connCommand.Parameters.AddWithValue("@HotelRoomTypeID", Convert.ToInt32(ddlRoomTypes.SelectedValue));
+
+                connCommand.ExecuteNonQuery();
+            }
+        }
+
+        /// <summary>
+        /// Changes the roomID of the specified reservation detail id without
+        /// modifying the rack rate and any applied discounts
+        /// </summary>
+        /// <param name="roomID">New roomID to change to</param>
+        private void updateKeepDiscount(int roomID)
+        {
+            using (SqlConnection conn = new SqlConnection(
+                ConfigurationManager.ConnectionStrings["TreasureLandDB"].ConnectionString))
+            {
+                conn.Open();
+
+                string command = "UPDATE [ReservationDetail] SET [RoomID] = @RoomID " +
+                                    "WHERE [ReservationDetailID] = @ReservationDetailID";
+                SqlCommand connCommand = new SqlCommand(command, conn);
+
+                connCommand.Parameters.AddWithValue("@RoomID", roomID);
+                connCommand.Parameters.AddWithValue("@ReservationDetailID", Convert.ToInt32(Session["ResDetailID"].ToString()));
+
+                connCommand.ExecuteNonQuery();
+            }
+        }
+    }
+}
