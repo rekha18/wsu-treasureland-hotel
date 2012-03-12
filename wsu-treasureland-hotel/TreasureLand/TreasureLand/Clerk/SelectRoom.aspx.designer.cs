194b77ab721c306c3445850847d02e48ff08d6bb
jimmywhip <jimmywhip@gmail.com>
2012-03-12 04:25:41 +0000

    Added GridView to show selected rooms

--- wsu-treasureland-hotel/TreasureLand/TreasureLand/Clerk/SelectRoom.aspx.designer.cs
+++ wsu-treasureland-hotel/TreasureLand/TreasureLand/Clerk/SelectRoom.aspx.designer.cs
@@ -58,6 +58,15 @@ namespace TreasureLand.Clerk {
         protected global::System.Web.UI.WebControls.Button btnUpdateCheckedRoomsList;
         
         /// <summary>
+        /// gvSelectedRooms control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.GridView gvSelectedRooms;
+        
+        /// <summary>
         /// lblNoOpenRooms control.
         /// </summary>
         /// <remarks>
